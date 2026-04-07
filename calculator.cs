9b9807f7a4157174fc9771ac7ff9dfcba8bf476e
Muhammad Febrian Eka Yulianto <muhfebrianey@gmail.com>
2026-04-07 22:08:38 +0700

    Tugas 1 sampai 4 selesai

---
+++ calculator.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Calculator
+{
+    static void Main(string[] args)
+    {
+        int a = 10;
+        int b = 6;
+
+        Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
+        Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
+
+        Console.WriteLine("\nPress any key to exit");
+        Console.ReadKey();
+    }
+
+    static int Penambahan(int a, int b)
+    {
+        return a + b;
+    }
+
+    static int Pengurangan(int a, int b)
+    {
+        return a - b;
+    }
+}
